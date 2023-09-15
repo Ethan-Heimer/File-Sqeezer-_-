e7778b9ed750eeef001f47a4af1399661b6cbefd
Ethan-Heimer <111470266+Ethan-Heimer@users.noreply.github.com>
2023-09-15 14:14:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const upload = multer()
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
